c0ec7c5b060abcf31bcc74ba20e99edc9e513139
kyeongseoj <kyeongseojeon@likelion.org>
2021-10-12 01:08:15 +0900

    10/12 중간점검

--- Schatz/urls.py
+++ Schatz/urls.py
@@ -17,18 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
-
-# urlpatterns = [
-#     path('', include('beer.urls')),
-#     path('admin/', admin.site.urls),
-#     path('beer/', include('beer.urls')),
-#     path('board/', include('board_app.urls')),
-# ]
+from pybo.views import base_views
 
 urlpatterns = [
     path('', include('beer.urls')),
     path('admin/', admin.site.urls),
-    # path('beer/', include('beer.urls')),
-    path('board/', include('board_app.urls')),
+    path('pybo/', include('pybo.urls'), name='pybo'),
+    path('common/', include('common.urls')),
+    path('index/', base_views.index, name='index'),  # '/' 에 해당되는 path
 ]
